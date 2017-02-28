66853cce414b4173914339922f45790cddf1d58b
sinderpl <sinderpl@gmail.com>
2017-02-28 12:38:16 +0000

    Exploring all possible branches in the tree

--- main.rkt
+++ main.rkt
@@ -8,12 +8,13 @@
 
       ;Initialise variables
 ;List of the six numbers, not random for now
-(define l (list 1 6 ))
+(define l (list 2 3 ))
 ;The numerical answer for the equations to calculate it
-(define answerNumber 20)
+(define answerNumber 3)
 ;Define the possible equation list
 (define answerEquations (list))
 (define signList (list "+" "-" "*" "/" ))
+(define permCount 0)
 
 
 
@@ -22,36 +23,52 @@
      ;Initial method for beginning the search
 (define(findAnswers originalList oAnswer )
   
-  ;Outer for loop iterating through each number in list
-  (for([currentNumber originalList])
-    
-    ;Inner for loop applying each sign to the equation
-    (for ([sign signList])
-      
-      ;Define the current equation
-      (define currentEq (createEquation oAnswer sign currentNumber));
+  (define sets(permutations originalList)
+    )
+  
+  (for ([set sets])
+    (display set)
+    ;Outer for loop iterating through each number in list
+    (for([currentNumber set])
+      (set! originalList (remove currentNumber originalList))
+      ;Inner for loop applying each sign to the equation
+      (for ([sign signList])
+        
+        ;Define the current equation
+        (define currentEq (createEquation oAnswer sign currentNumber));
+        
+        ;Calculate the current Equation
+        (define currentAnswer (eval (read (open-input-string currentEq)) ns))
 
-      ;Calculate the current Equation
-      (define currentAnswer (eval (read (open-input-string currentEq)) ns))
-    
-      ;Display the calculation
-      (display (string-append (~v currentEq) " = "(~v currentAnswer) "\n") )
-      
-      
-      ;Evaluate if answer is equal to the answer number
-      (if ( = currentAnswer answerNumber)
-          (display (string-append  " success" "\n" )) (printf (string-append " failure" "\n" ))) ;TODO return)
-      ;Evaluate if the answer is a negative number or a fraction
-      (if (exact-positive-integer? currentAnswer )
-           (display  "continue calculations \n") (display "answer is uneven, return \n"));TODO return
-     ; )
-      ;Evaulate whether the list is empty and if so call the same function recursively
-      ;TODO loop function
-      
-      )
-    ;(or ((modulo currentAnswer 2) not 0))
-    ; (if (<= currentAnswer 0 )
+        ;Evaluate if answer is equal to the answer number
+        (if ( = currentAnswer answerNumber)
+            (begin
+              (display (string-append  " success" "\n" ))
+              (display (string-append (~v currentEq) " = "(~v currentAnswer) "\n") )
+              (set! permCount (+ permCount 1)))
+            
+            (printf (string-append " failure" "\n" ))) ;TODO return)
+        
+        ;Evaluate if the answer is a negative number or a fraction
+        (if (exact-positive-integer? currentAnswer )
+            (begin
+              ;Display the calculation
+              ;(display (string-append (~v currentEq) " = "(~v currentAnswer) "\n") )
+              (findAnswers originalList currentAnswer))
+            
+            (begin
+              ;Display the calculation
+              ;(display (string-append (~v currentEq) " = "(~v currentAnswer) "\n") )
+              display "Return \n"));TODO return
+        ; )
+        ;Evaulate whether the list is empty and if so call the same function recursively
+        ;TODO loop function
+        
+        )
+      ;(or ((modulo currentAnswer 2) not 0))
+      ; (if (<= currentAnswer 0 )
   )
+ )
 )
 
      ;Method for calculating the current equation
@@ -61,5 +78,6 @@
 
 ;Call the main search method
 ( findAnswers l 1 )
+(display permCount)
 
 
